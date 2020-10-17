f2ed6111592b7de3a4797c395e78010d8b5f9418
David Dowd <ddowd97@gmail.com>
2020-10-17 15:35:02 -0500

    Altered Listing.. Again.. Again..

--- P2-Angular/src/app/landing/landing.component.ts
+++ P2-Angular/src/app/landing/landing.component.ts
@@ -92,16 +92,16 @@ export class LandingComponent implements OnInit {
     for(let i=0; i< this.users.length; i++)
     {
       // Animals, Art, Nightlife, Beauty, Learning, Entertainment, Religion, Shopping, HomeDecour, Fitness
-      if (this.users[i].preferencesModel.animals == this.user.preferencesModel.animals ||
-          this.users[i].preferencesModel.art == this.user.preferencesModel.art ||
-          this.users[i].preferencesModel.nightlife == this.user.preferencesModel.nightlife ||
-          this.users[i].preferencesModel.beauty == this.user.preferencesModel.beauty ||
-          this.users[i].preferencesModel.learning == this.user.preferencesModel.learning ||
-          this.users[i].preferencesModel.entertainment == this.user.preferencesModel.entertainment ||
-          this.users[i].preferencesModel.religion == this.user.preferencesModel.religion ||
-          this.users[i].preferencesModel.shopping == this.user.preferencesModel.shopping ||
-          this.users[i].preferencesModel.homedecour == this.user.preferencesModel.homedecour ||
-          this.users[i].preferencesModel.fitness == this.user.preferencesModel.fitness)
+      if (((this.users[i].preferencesModel.animals == this.user.preferencesModel.animals) && (this.users[i].preferencesModel.animals == true)) ||
+         ((this.users[i].preferencesModel.art == this.user.preferencesModel.art) && (this.users[i].preferencesModel.art == true)) ||
+         ((this.users[i].preferencesModel.beauty == this.user.preferencesModel.beauty) && (this.users[i].preferencesModel.beauty == true)) ||
+         ((this.users[i].preferencesModel.entertainment == this.user.preferencesModel.entertainment) && (this.users[i].preferencesModel.entertainment == true)) ||
+         ((this.users[i].preferencesModel.fitness == this.user.preferencesModel.fitness) && (this.users[i].preferencesModel.fitness == true)) ||
+         ((this.users[i].preferencesModel.homedecour == this.user.preferencesModel.homedecour) && (this.users[i].preferencesModel.homedecour == true)) ||
+         ((this.users[i].preferencesModel.learning == this.user.preferencesModel.learning) && (this.users[i].preferencesModel.learning == true)) ||
+         ((this.users[i].preferencesModel.nightlife == this.user.preferencesModel.nightlife) && (this.users[i].preferencesModel.nightlife == true)) ||
+         ((this.users[i].preferencesModel.religion == this.user.preferencesModel.religion) && (this.users[i].preferencesModel.religion == true)) ||
+         ((this.users[i].preferencesModel.shopping == this.user.preferencesModel.shopping) && (this.users[i].preferencesModel.shopping == true)))
       {
         this.filteredUsers.push(this.users[i]);
       }
