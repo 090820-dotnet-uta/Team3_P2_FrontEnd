652e148bbc0e2f5b6ff9ceb1f790b3e003d48d7f
David Dowd <ddowd97@gmail.com>
2020-10-19 16:21:56 -0500

    Further Revisions

--- P2-Angular/src/app/app-routing.module.ts
+++ P2-Angular/src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-// import { AuthGuardService } from './auth-guard.service';
+import { AuthGuardService } from './auth-guard.service';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes =
 [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: HomeComponent },
-    { path: 'register', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'landing', component: HomeComponent}
+    { path: '', component: HomeComponent }
 ];
 
 @NgModule({
